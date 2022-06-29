49b033eb927c1678e2e6c4928b8ad88362d1562b
Hank <hank.tsunghanlin@gmail.com>
2022-06-29 16:32:48 +0800

    06291632

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,38 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div id="nav">
+      <router-link to="/all">全部</router-link> |
+      <router-link to="/active">未完成</router-link> |
+      <router-link to="/complete">已完成</router-link>
+    </div>
+    <TodoInput />
+    <div id="list">
+      <TodoListItem v-for="index in todoIndex" :key="index" :index="index" />
+    </div>
   </div>
 </template>
 
-<style>
+<script>
+import TodoInput from "@/components/TodoInput/TodoInput.vue";
+import TodoListItem from "@/components/TodoListItem/TodoListItem.vue";
+
+export default {
+  components: {
+    TodoInput,
+    TodoListItem,
+  },
+  mounted() {
+    this.$store.dispatch("INIT_TODOS");
+  },
+  computed: {
+    todoIndex() {
+      return this.$store.getters["todoIndex"];
+    },
+  },
+};
+</script>
+
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -17,16 +41,18 @@
   color: #2c3e50;
 }
 
-nav {
+#nav {
   padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+  a.router-link-exact-active {
+    color: #42b983;
+  }
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-color: bisque;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,72 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const LS = {
+  load() {
+    return JSON.parse(localStorage.getItem('vue_todos') || '[]')
+  },
+  save(data) {
+    localStorage.setItem('vue_todos', JSON.stringify(data))
+  }
+
+}
+const filter = {
+  all(todos) {
+    return todos
+  },
+  active(todos) {
+    return todos.filter(todo => {
+      return !todo.complete
+    })
+  },
+  complete(todos) {
+    return todos.filter(todo => {
+      return todo.complete
+    })
+  }
+}
+
 export default new Vuex.Store({
+  strict: true,
   state: {
+    todos: [
+
+
+
+    ]
   },
   getters: {
+    todoIndex(state) {
+      return filter[state.route.name](state.todos).map(todo => state.todos.indexOf(todo))
+    }
   },
   mutations: {
+    SET_TODOS(state, data) {
+      state.todos = data
+      LS.save(state.todos)
+    },
+    ADD_TODO(state, data) {
+      state.todos.push(data)
+      LS.save(state.todos)
+    },
+    UPDATE_TODO(state, { index, data }) {
+      state.todos[index].complete = data.complete
+      state.todos[index].content = data.content
+      LS.save(state.todos)
+
+    },
+    REMOVE_TODO(state, index) {
+      state.todos.splice(index, 1)
+      LS.save(state.todos)
+
+    }
   },
   actions: {
+    INIT_TODOS({ commit }) {
+      // 讀取localStorage
+
+      commit('SET_TODOS', LS.load())
+    }
   },
-  modules: {
-  }
+
 })
